12b3806dc38916d40c03cc676912e9c834b062b5
Eric Silk <eric_silk@selinc.com>
2019-05-17 15:58:32 -0700

    Did the breaking change to bonnie and clyde, and adjusted formatting to my preferred style

--- ps5/bonnie_and_clyde.cpp
+++ ps5/bonnie_and_clyde.cpp
@@ -21,40 +21,44 @@ size_t bank_balance = 300;
 static std::mutex atm_mutex;
 static std::mutex msg_mutex;
 
-size_t withdraw(const std::string& msg, size_t amount) {
-  std::lock(atm_mutex, msg_mutex);
-  std::lock_guard<std::mutex> message_lock(msg_mutex, std::adopt_lock);
+size_t withdraw(const std::string& msg, size_t amount)
+{
+      std::lock(atm_mutex, msg_mutex);
+      std::lock_guard<std::mutex> message_lock(msg_mutex, std::adopt_lock);
 
-  std::cout << msg << " withdraws " << std::to_string(amount) << std::endl;
+      std::cout << msg << " withdraws " << std::to_string(amount) << std::endl;
 
-  std::lock_guard<std::mutex> account_lock(atm_mutex, std::adopt_lock);
+      std::lock_guard<std::mutex> account_lock(atm_mutex, std::adopt_lock);
 
-  bank_balance -= amount;
-  return bank_balance;
+      bank_balance -= amount;
+      return bank_balance;
 }
 
-size_t deposit(const std::string& msg, size_t amount) {
-  std::lock(atm_mutex, msg_mutex);
-  std::lock_guard<std::mutex> account_lock(atm_mutex, std::adopt_lock);
+size_t deposit(const std::string& msg, size_t amount)
+{
+      std::lock(atm_mutex, msg_mutex);
+      std::lock_guard<std::mutex> account_lock(atm_mutex, std::adopt_lock);
 
-  bank_balance += amount;
+      bank_balance += amount;
 
-  std::lock_guard<std::mutex> message_lock(msg_mutex, std::adopt_lock);
+      std::lock_guard<std::mutex> message_lock(msg_mutex, std::adopt_lock);
 
-  std::cout << msg << " deposits " << std::to_string(amount) << std::endl;
-  return bank_balance;
+      std::cout << msg << " deposits " << std::to_string(amount) << std::endl;
+      return bank_balance;
 }
 
-int main() {
-  std::cout << "Starting balance is " << bank_balance << std::endl;
+int main()
+{
+      std::cout << "Starting balance is " << bank_balance << std::endl;
 
-  std::future<size_t> bonnie = std::async(std::launch::async, withdraw, "Bonnie", 100);
-  std::future<size_t> clyde  = std::async(std::launch::async, deposit, "Clyde", 100);
+      std::future<size_t> bonnie = std::async(std::launch::async, withdraw, "Bonnie", 100);
+      std::future<size_t> clyde  = std::async(std::launch::async, deposit, "Clyde", 100);
 
-  std::cout << "Balance after Clyde's deposit is " << clyde.get() << std::endl;
-  std::cout << "Balance after Bonnie's withdrawal is " << bonnie.get() << std::endl;
+      std::cout << "Balance after Clyde's deposit is " << clyde.get() << std::endl;
+      std::cout << "Balance after Bonnie's withdrawal is " << bonnie.get() << std::endl;
+      std::cout << "Double checking Clyde's deposit" << clyde.get() << std::endl;
 
-  std::cout << "Final bank balance is " << bank_balance << std::endl;
+      std::cout << "Final bank balance is " << bank_balance << std::endl;
 
-  return 0;
+      return 0;
 }
